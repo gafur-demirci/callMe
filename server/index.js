21b70685cdf2fbca51147b60f0032d438ebcd210
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-12 22:47:36 +0300

    Refactor socket.io server setup to include CORS configuration and improve logging messages

--- server/index.js
+++ server/index.js
@@ -1,31 +1,27 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
-io.on('connection', (socket) => {
-  console.log('Yeni kullanıcı bağlandı:', socket.id);
+io.on("connection", (socket) => {
+  console.log("Bağlandı:", socket.id);
 
-  socket.on('offer', (data) => {
-    socket.broadcast.emit('offer', data);
-  });
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+  socket.on("ice-candidate", (data) => socket.broadcast.emit("ice-candidate", data));
 
-  socket.on('answer', (data) => {
-    socket.broadcast.emit('answer', data);
-  });
-
-  socket.on('ice-candidate', (data) => {
-    socket.broadcast.emit('ice-candidate', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Kullanıcı ayrıldı:', socket.id);
+  socket.on("disconnect", () => {
+    console.log("Ayrıldı:", socket.id);
   });
 });
 
 server.listen(3000, () => {
-  console.log('Sinyal sunucusu çalışıyor: http://localhost:3000');
+  console.log("Sinyal sunucusu çalışıyor: http://localhost:3000");
 });
