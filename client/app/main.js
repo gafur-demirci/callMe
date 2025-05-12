9c73dd966bbae6ce79b83a28c6935890ec062047
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-12 22:19:16 +0300

    Refactor main.js to enhance security by enabling context isolation and disabling node integration

--- client/app/main.js
+++ client/app/main.js
@@ -1,13 +1,15 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      contextIsolation: false,
-      nodeIntegration: true,
-    },
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
   });
 
   win.loadFile("index.html");
