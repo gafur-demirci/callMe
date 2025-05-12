e931ca2f5519e55ee36a0f6b5095693498f04c91
Abdulgafur Demirci <gafur.gs.68@gmail.com>
2025-05-12 22:19:31 +0300

    Add initial implementation of socket.io server for real-time communication

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('Yeni kullanıcı bağlandı:', socket.id);
+
+  socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.broadcast.emit('ice-candidate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Kullanıcı ayrıldı:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Sinyal sunucusu çalışıyor: http://localhost:3000');
+});
